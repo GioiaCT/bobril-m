5f11507b56a2fb892a35cd2b94345725f491303e
Matej Knapovsky <matej.knapovsky@gmail.com>
2021-04-08 16:45:13 +0200

    BM-0001 - created typography component

---
+++ src/typography.ts
@@ -0,0 +1,26 @@
+import * as b from "bobril";
+
+export const enum Typogtype {
+    h1 = "h1", h2 = "h2", h3 = "h3", h4 = "h4", h5 = "h5", h6 = "h6"
+}
+
+export interface ITypographyData {
+    children: b.IBobrilChildren;
+    tag: Typogtype;
+}
+
+interface ICtx extends b.BobrilCtx<ITypographyData>{
+    data: ITypographyData;
+}
+
+export const Typography = b.createComponent<ITypographyData>({
+    render(ctx: ICtx, me: b.IBobrilNode) {
+        me.tag = ctx.data.tag;
+        me.children = {tag: ctx.data.tag, children: ctx.data.children };
+        b.style(me, Typographystyle);
+    }
+});
+
+const Typographystyle = b.styleDef( {
+    textAlign: "left",
+});
